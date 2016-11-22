bb444cd9b65f8975495fb12f2b815a6b389792b6
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2016-11-22 11:08:48 +0800

    read 12 data

--- app/views/index.py
+++ app/views/index.py
@@ -4,13 +4,14 @@ from django.views.decorators.csrf import csrf_exempt
 from dss.Serializer import serializer
 import json
 from objects.AqiParameter import AqiParameter
+from datetime import datetime, timedelta
 
 
 def index(request):
 	# 获取所有观测点的数据
 	datas_list = [
-		{"name": u"京师方圆","so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158},
-		{"name": u"清风大道路","so2": 33, "no2": 20, "pm10": 20, "co": 2, "o3": 12, "pm25": 12}
+		{"name": u"京师方圆","so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-12 12:00:00"},
+		{"name": u"清风大道路","so2": 33, "no2": 20, "pm10": 20, "co": 2, "o3": 12, "pm25": 12, "time": "2016-11-12 12:00:00"}
 	]
 	# data = [
 	# 	{
@@ -57,7 +58,63 @@ def index(request):
 	average_data["Main_Pollute_1"] = calculator.Main_Pollute_1
 	average_data["AQI_info_1"] = calculator.AQI_info_1
 	# 计算最近24小时站点均值
-
+	# 首先获取最近24小时内所有站点的的数据
+	datas_list_12 = [
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:00:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:10:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:20:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:30:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:40:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 11:00:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 11:57:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 11:13:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 11:35:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 10:57:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 10:13:00"},
+		{"name": u"京师方圆", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 10:35:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:00:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:10:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:20:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:30:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 12:40:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 11:00:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 11:57:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 11:13:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 11:35:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 10:57:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 10:13:00"},
+		{"name": u"清风大道路", "so2": 32, "no2": 53, "pm10": 294, "co": 2, "o3": 33, "pm25": 158, "time": "2016-11-22 10:35:00"},
+	]
+	average_12 = []
+	import time
+	time_now = time.localtime(time.time())
+	time_now_hour = time.localtime(time.time()).tm_hour
+	print time_now
+	twelve_data = []
+	twelve_data_hour = []
+	twelve_data_data = {"so2":[], "no2":[], "pm10":[], "co":[], "o3":[], "pm25":[]}
+	factors = ["so2", "no2", "pm10", "co", "o3", "pm25"]
+	for i in range(1, time_now_hour + 1):
+		twelve_data_hour.append(i)
+		time_start = datetime(time_now.tm_year, time_now.tm_mon, time_now.tm_mday, i, 0, 0)
+		time_end = datetime(time_now.tm_year, time_now.tm_mon, time_now.tm_mday, i, 0, 0) + timedelta(hours=1)
+		print "start", time_start
+		print "end", time_end
+		for factor in factors:
+			factor_sum = 0
+			factor_num = 0
+			for data in datas_list_12:
+				time = datetime.strptime(data["time"], "%Y-%m-%d %H:%M:%S")
+				print time
+				if time_start <= time < time_end:
+					factor_sum += data[factor]
+					factor_num += 1
+			try:
+				twelve_data_data[factor].append(factor_sum/factor_num)
+			except:
+				twelve_data_data[factor].append(0)
+	print twelve_data_hour
+	print twelve_data_data
 	return render(request, "app/index.html", {
 		"average_data": average_data,
 		"data_real_time": data_real_time,
