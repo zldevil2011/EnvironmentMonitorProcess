6a5aa52ad7797f7e35fb5107f42acca2015927bc
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2016-12-22 09:59:42 +0800

    fix bug

--- app/views/historical.py
+++ app/views/historical.py
@@ -156,7 +156,7 @@ def historical_data_list(request,device_id):
 		if start_time is None:
 			start_time = datetime(today.year, today.month, today.day, 0, 0, 0)
 			start_time = str(start_time)[0:10]
-			return HttpResponseRedirect("/historical_device_data_list/" + device["id"] + "?data_time=" + start_time)
+			return HttpResponseRedirect("/historical_device_data_list/" + str(device["id"]) + "?data_time=" + start_time)
 			end_time = start_time + timedelta(days=1)
 		else:
 			start_time += " 00:00:00"
