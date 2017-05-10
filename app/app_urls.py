b375a43ba490dfd83102a02e2b1d5d0de4bbba8c
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2017-05-10 17:45:06 +0800

    add ranking for app

--- app/app_urls.py
+++ app/app_urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
 	url(r'^index/$', TemplateView.as_view(template_name="phone/index.html")),
+	url(r'^ranking/$', TemplateView.as_view(template_name="phone/ranking.html")),
 ]
