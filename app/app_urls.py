b2f15f10c6409db74fb665dd1d3e14190ca3f541
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2017-05-12 17:31:24 +0800

    ranking

--- app/app_urls.py
+++ app/app_urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
-from views.phone.index import index, map
+from views.phone.index import index, map, ranking
 from views.phone.user import login, logout, information
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
 	url(r'^user/login/$', login, name="app_user_login"),
 	url(r'^user/logout/$', logout, name="app_user_logout"),
 	url(r'^user/information/$', information, name="app_user_information"),
-	url(r'^ranking/$', TemplateView.as_view(template_name="phone/ranking.html")),
+	url(r'^ranking/$', ranking, name="app_ranking"),
 	url(r'^map/$', map, name="app_map"),
 	url(r'^mine/$', TemplateView.as_view(template_name="phone/mine.html")),
 	url(r'^aboutUs/$', TemplateView.as_view(template_name="phone/aboutUs.html")),
